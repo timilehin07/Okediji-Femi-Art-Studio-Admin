58029faebc038be4b02c9ce65f50c8e919adfebd
Afolayan Tim <afolayantimi555@gmail.com>
2026-07-06 13:52:18 +0100

    Update work.ts

--- schemaTypes/work.ts
+++ schemaTypes/work.ts
@@ -13,6 +13,17 @@ export default defineType({
       validation: Rule => Rule.required(),
     }),
 
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+      validation: Rule => Rule.required(),
+    }),
+
     /**
      * 🔥 MULTIPLE IMAGES (Front, Side, Back, etc.)
      */
