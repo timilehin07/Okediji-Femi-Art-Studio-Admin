e3786fd98efa301263ca00a6e56f38a143ac42cf
Afolayan Tim <afolayantimi555@gmail.com>
2026-07-11 22:19:43 +0100

    Add new schema types for studio, process, and processStep

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,5 +1,8 @@
 import home from "./home"
 import about from "./about"
 import work from "./work"
+import studio from "./studio"
+import process from "./process"
+import processStep from "./processStep"
 
-export const schemaTypes = [home, about, work]
+export const schemaTypes = [home, about, work, studio, process, processStep]
