b27bda9d90b6406540f5f21adfc9755ae4971702
Afolayan Tim <afolayantimi555@gmail.com>
2026-07-12 22:01:17 +0100

    Import studio, process, and processStep using named imports

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,9 +1,10 @@
 import home from "./home"
 import about from "./about"
 import work from "./work"
-import studio from "./studio"
-import process from "./process"
-import processStep from "./processStep"
+
+import { studio } from "./studio"
+import { process } from "./process"
+import { processStep } from "./processStep"
 import { commissionWork } from "./commissionWork"
 
 export const schemaTypes = [home, about, work, studio, process, processStep, commissionWork]
