4b50e7e7fa8fa39d0f29fdf258b88120ce4ee854
Afolayan Tim <afolayantimi555@gmail.com>
2026-07-12 21:44:33 +0100

    Add commissionWork to schemaTypes export

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -4,5 +4,6 @@ import work from "./work"
 import studio from "./studio"
 import process from "./process"
 import processStep from "./processStep"
+import { commissionWork } from "./commissionWork"
 
-export const schemaTypes = [home, about, work, studio, process, processStep]
+export const schemaTypes = [home, about, work, studio, process, processStep, commissionWork]
